d9d9c4f68c7ccf00495e6744a405984b476411b4
zhangzhaolin <626344962@qq.com>
2017-11-01 14:10:31 +0800

    spring之旅

--- 第一章 spring之旅/Book查询-Spring模板封装/src/BookMain.java
+++ 第一章 spring之旅/Book查询-Spring模板封装/src/BookMain.java
@@ -7,10 +7,11 @@ import org.springframework.jdbc.core.JdbcTemplate;
  */
 public class BookMain {
 
-    public static void main(String args[]){
+    public static void main(String []args){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         JdbcTemplate jdbcTemplate =  context.getBean(JdbcTemplate.class);
         BookJdbcTemplate template = new BookJdbcTemplate();
+
         try {
             Book book = template.getBookById(jdbcTemplate,1L);
             System.out.println(book);

d56b5b45a4259d60884aebf29e7da930023743d9
zhangzhaolin <626344962@qq.com>
2017-11-09 09:04:52 +0800

    灵活的探险骑士-构造器注入-XML注入

--- 第一章 spring之旅/灵活的探险骑士-构造器注入-XML配置/src/com/springinaction/KnightMain.java
+++ 第一章 spring之旅/灵活的探险骑士-构造器注入-XML配置/src/com/springinaction/KnightMain.java
@@ -10,8 +10,8 @@ public class KnightMain {
 
     public static void main(String []args){
         // 通过xml的方式 加载spring上下文
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("knight.xml");
-        // 获取 knight bean
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/springinaction/knights.xml");
+        // 获取 knight bea
         Knight knight = context.getBean(Knight.class);
         knight.embarkOnQuest();
         context.close();

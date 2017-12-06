ee60da0030b02a12f74b065fe9eaba4775d9f913
zhangzhaolin <zhangzhaolin@qq.com>
2017-12-06 15:10:17 +0800

    一个简单的profile的例子

---
+++ 第三章 高级装配/一个简单的例子profile/src/main/java/MainTest.java
@@ -0,0 +1,25 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+
+/**
+ * @author shiwa
+ */
+
+public class MainTest {
+
+    public static void main(String []args){
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+
+        context.getEnvironment().setActiveProfiles("dev");
+        context.register(ProfileConfig.class);
+        context.refresh();
+
+        DemoBean demoBean = context.getBean(DemoBean.class);
+
+        System.out.println(demoBean.getContent());
+
+        context.close();
+
+    }
+}

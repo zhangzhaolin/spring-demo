44f0a370f0db82de7e4c8ff91e9538adfc76c404
张肇霖 <zhangzhaolin@zhangzhaolindeMacBook-Air.local>
2018-08-05 20:07:56 +0800

    update

--- 第三章 高级装配/使用会话和请求作用域/src/main/java/demo/test/ShoppingCart.java
+++ 第三章 高级装配/使用会话和请求作用域/src/main/java/demo/test/ShoppingCart.java
@@ -9,7 +9,7 @@ import org.springframework.web.context.WebApplicationContext;
  * @author shiwa
  */
 @Component
-@Scope(value = WebApplicationContext.SCOPE_SESSION,
+@Scope(value = WebApplicationContext.SCOPE_REQUEST,
     proxyMode = ScopedProxyMode.TARGET_CLASS
 )
 public interface ShoppingCart {
---
+++ 第三章 高级装配/注入外部的值-属性占位符/src/main/java/com/springdemo/Person.java
@@ -0,0 +1,25 @@
+package com.springdemo;
+
+public class Person {
+
+    // 成员
+    private String name;
+    private int age;
+
+    // 方法
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
